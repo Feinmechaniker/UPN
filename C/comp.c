566c952fe8cb527da2c2a6b2888df0c6a2782d72
srswift7 <48880723+srswift7@users.noreply.github.com>
2019-05-31 17:40:09 +0200

    index commands added, some refactoring
    Beside some bugfixes and the introduction of the index commands, the input handling of the code was improved to support spaces within code (e.g. IF X>0),

--- C/comp.c
+++ C/comp.c
@@ -33,6 +33,7 @@ int statflag_file_output;
 int statflag_verbose_mode;
 int statflag_fill_nop_mode;
 int statflag_boris4_mode;
+int max_prg;
 
 
 /* Funktionsprototypen */
@@ -53,6 +54,7 @@ void usage(char * Program);
 int isWspace (char p_zeichen)  // IN: char to check
 {
   if ( (p_zeichen == ' ') || 
+       (p_zeichen == '@') || 
        (p_zeichen == '\t') || 
        (p_zeichen == '\r') || 
        (p_zeichen == '\f') || 
@@ -94,13 +96,22 @@ int is_comment(char *line) {
 // --------------------------------------------------
 // Welche Kommandos brauchen eine Adresse ?
 int needs_adress(char code) {
+
+char bas_code;
+
+   // Die Index-codes sind um 128 verschoben
+   if (code < 0)
+      bas_code = code+128;
+   else
+      bas_code = code;
+
    if (statflag_boris4_mode) {
-       if (code ==  7 || code == 8 || code == 10 || code == 11 || code == 18 || code == 19 || code == 20 \
-                      || code == 21 || code == 69 || code == 74 || code == 75 || code == 76 || code == 112 || code == 126) return 1;
+       if (bas_code ==  7 || bas_code == 8 || bas_code == 10 || bas_code == 11 || bas_code == 18 || bas_code == 19 || bas_code == 20 \
+                      || bas_code == 21 || bas_code == 69 || bas_code == 74 || bas_code == 75 || bas_code == 76 || bas_code == 112 || bas_code == 126) return 1;
    } else {
-       if (code == 7 || code == 8 || code == 10 || code == 11 || code == 18 || code == 19 || code == 20 \
-                      || code == 21 || code == 69 || code == 74 || code == 75 || code == 76 || code == 112 || code == 116 \
-                      || code == 117 || code == 118 || code == 119 || code == 120 || code == 121 || code == 126) return 1;
+       if (bas_code == 7 || bas_code == 8 || bas_code == 10 || bas_code == 11 || bas_code == 18 || bas_code == 19 || bas_code == 20 \
+                      || bas_code == 21 || bas_code == 69 || bas_code == 74 || bas_code == 75 || bas_code == 76 || bas_code == 112 || bas_code == 116 \
+                      || bas_code == 117 || bas_code == 118 || bas_code == 119 || bas_code == 120 || bas_code == 121 || bas_code == 126) return 1;
    }
    return 0;
 }
@@ -108,27 +119,73 @@ int needs_adress(char code) {
 
 // --------------------------------------------------
 // Kommandocodes umsetzen
+// Input ist ein Pointer auf den Kommandostring, der aber Leerzeichen enthalten kann
+// Wenn er mindestens ein Leerzeichen hat, enthaelt er auch eine Adresse.
+// Die Adresse muss vor dem Zeichenkettenvergleich weggetrimmt werden
 unsigned short get_cmd_cde(char * code) {
   int i;
+  char * last_space;
+  char wrk_kdo[256];
 
-  for(i=0;i<128;i++) {
-     if (statflag_boris4_mode) {
-        if (!strcmp(code, kdo_4_codes[i])) return i;
-     } else {
-        if (!strcmp(code, kdo_5_codes[i])) return i;
+  strcpy(wrk_kdo, code);
+
+  last_space = strrchr(wrk_kdo, ' '); 
+
+  if (last_space != NULL) {
+     // trimmen
+    while (isWspace(*last_space) && last_space > wrk_kdo) {
+       *last_space = '\0';
+       last_space--;
+    }
+  }
+
+  if (statflag_boris4_mode) {
+     for(i=0; i<128; i++) {
+        if (!strcmp(wrk_kdo, kdo_4_codes[i])) return i;
+     }
+  } else {
+     for(i=0; i<256; i++) {
+        if (!strcmp(wrk_kdo, kdo_5_codes[i])) return i;
      }
   }
+
   fprintf(stderr, "ERROR: Kommando >%s< kann nicht uebersetzt werden\n", code);
+
   return 255;
+}
+
+// Codezeile ein wenig aufbereiten
+// trimright
+// toupper
+
+void clean_code_line(char *line) {
+   char * cp;
+
+   // toupper
+   for (cp = line; *cp != '\0' && *cp != ';'; cp++) {
+      *cp = toupper(*cp);
+   }
+
+   // Hinter einem ; kann alles abgeschnitten werden
+   if (*cp == ';') *cp = '\0';
+
+   // Trimmen
+   cp--;
+   while (isWspace (*cp)) {
+      *cp = '\0';
+      cp--;
+   }
+
 }
 
 
 void usage(char * Program) {
    fprintf(stderr, "%s - Ein Kommandozeilen Assembler fuer boris4/5 - Programme\n\n",Program);
-   fprintf(stderr, "Bitteschoen: %s [-v] [-n] [-4] [-o outfile] Eingabedatei\n",Program);
+   fprintf(stderr, "Bitteschoen: %s [-v] [-n] [-4] [-s startadresse] [-o outfile] Eingabedatei\n",Program);
    fprintf(stderr, "     -n          Datei mit NOP auffuellen\n");
    fprintf(stderr, "     -v          geschwaetziger modus\n");
    fprintf(stderr, "     -4          boris4 - modus (default: boris5)\n");
+   fprintf(stderr, "     -s Adresse  Startadresse des Programms (default: 0)\n");
    fprintf(stderr, "     -o Datei    Ausgabe in eine Datei (default: stdout)\n");
 }
 
@@ -137,9 +194,13 @@ int main(int argc, char * argv[]) {
      FILE* file_in;
      FILE* file_out;
      char line[256];
-     char kdo[8];
+     // char kdo[8];
      char *s;
+     char *cp;
      int c, i, a;
+
+     int startadr;
+     char startadr_str[256];
 
      int adresse;
      int zeile;
@@ -157,11 +218,12 @@ int main(int argc, char * argv[]) {
      statflag_verbose_mode = 0;
      statflag_fill_nop_mode = 0;
      statflag_boris4_mode = 0;
+     startadr_str[0] = '\0';
+     startadr = 0;
 
      s = line;
-     a = 0;
 
-     while ((c = getopt (argc, argv, (const char *) "nh4vo")) != -1) {
+     while ((c = getopt (argc, argv, (const char *) "nh4vo:s:")) != -1) {
         switch (c)
           {
           case 'v':
@@ -175,6 +237,24 @@ int main(int argc, char * argv[]) {
             break;
           case 'o':
             statflag_file_output = 1;
+            if (strlen(optarg) < 255) {
+                strcpy(outfile, optarg);
+            } else {
+                fprintf(stderr, "Parameterfehler Eingabedateiname -> %s\n",optarg);
+                return 1;
+            }
+            break;
+          case 's': // Startadresse folgt
+            if (strlen(optarg) < 255) {
+                strcpy(startadr_str, optarg);
+                i = sscanf(startadr_str, "%d", &startadr);
+                if (i < 1 || startadr > 255) {
+                   fprintf(stderr, "Parameterfehler Startadresse %s -> %d\n", startadr_str, startadr);
+                }
+            } else {
+                fprintf(stderr, "Parameterfehler Startadresse -> %s\n",optarg);
+                return 1;
+            }
             break;
           case 'h':
             usage(argv[0]);
@@ -189,6 +269,15 @@ int main(int argc, char * argv[]) {
           }
       }
 
+      index = optind;
+
+      if (index >= argc) {
+          fprintf (stderr, "Aufruffehler : Parameter\n");
+          usage(argv[0]);
+          return 1;
+      }
+      strcpy(infile, argv[index]);
+
       if (statflag_verbose_mode) {
          fprintf (stderr, "statflag_file_output = %d\n", statflag_file_output);
 
@@ -198,34 +287,19 @@ int main(int argc, char * argv[]) {
             fprintf (stderr, "Boris-5 modus\n");
          }
 
+         fprintf(stderr, "Files: Input: >%s< Output: >%s<\n", infile, outfile);
+         fprintf(stderr, "Startadresse: >%s< %d\n", startadr_str, startadr);
+
          for (index = optind; index < argc; index++) {
            fprintf (stderr, "Non-option argument %s\n", argv[index]);
          }
       }
 
-      index = optind;
-
-      if (index >= argc) {
-          fprintf (stderr, "Aufruffehler : Parameter\n");
-          usage(argv[0]);
-          return 1;
-      }
-
-      if (statflag_file_output) {
-        strcpy(outfile, argv[index]);
-        index++;
-        if (index >= argc) {
-          fprintf (stderr, "Aufruffehler : Parameter\n");
-          usage(argv[0]);
-          return 1;
-        }
-        strcpy(infile, argv[index]);
+      if (statflag_boris4_mode) {
+         max_prg = 100;
       } else {
-        strcpy(infile, argv[index]);
+         max_prg = 255;
       }
-
-      if (statflag_verbose_mode)
-         fprintf(stderr, "Files: Input: >%s< Output: >%s<\n", infile, outfile);
 
      if(strlen(infile) == 0) {
          usage(argv[0]);
@@ -251,15 +325,24 @@ int main(int argc, char * argv[]) {
          _setmode(_fileno(file_out), _O_BINARY);
 #endif
 
+         a = startadr;
 
          // printf("%c%c", 170, 83);
          while (fgets(s, 255, file_in) != NULL) {
             if (!is_comment(line)) {
+               clean_code_line(line);
                if (isWspace (line[0])){
-                  i = sscanf(s, " %s %d", kdo, &adresse);
-                  i++;
+                  // wir haben vorn keine Zeilennummer
+                  // i = sscanf(s, "%[^@]@%d", kdo, &adresse);
+                  zeile = 0;
+                  // vorspulen bis zum ersten "Nicht-Leerzeichen"
+                  cp = line;
+                  while (isWspace(*cp) && *cp != '\0') cp++;
+                  i = 1;
                } else {
-                  i = sscanf(s, "%d %s %d", &zeile, kdo, &adresse);
+                  // wir haben vorn eine Zeilennummer
+                  // i = sscanf(s, "%d%[^@]@%d", &zeile, kdo, &adresse);
+                  i = sscanf(s, "%d", &zeile);
                   // Fehlerbehandlung bzw Markenverschiebung
                   if (a != zeile) {
                       fprintf(stderr, "WARNING: Adressabweichung in Zeile %d (%d)\n", a, zeile);
@@ -272,14 +355,30 @@ int main(int argc, char * argv[]) {
                          fprintf(stderr, "ERROR: Adressabweichung in Zeile %d (%d) nicht korrigierbar\n", a, zeile);
                       }
                   }
+                  // vorspulen bis zum ersten "Nicht-Leerzeichen hinter der Zeilennummer"
+                  cp = line;
+                  while ((isWspace(*cp) == 0) && *cp != '\0') cp++;
+                  while (isWspace(*cp) && *cp != '\0') cp++;
                }
                
                a++;
-               code = get_cmd_cde(kdo);
+               code = get_cmd_cde(cp);
+               // if (statflag_verbose_mode)
+               //    fprintf (stderr, "Command >%s< translated into code = %d\n", cp, code);
+
+               i++;
+
                if (!needs_adress(code)) {
                   i++;
                   adresse = 0;
+               } else {
+                  // Adresse Lesen
+                  while (*cp != '\0') cp++;
+                  cp--;
+                  while ((isWspace(*cp) == 0)) cp--;
+                  i += sscanf(cp, "%d", &adresse);
                }
+
                if (i != 3) {
                   fprintf(stderr, "ERROR: Parameterfehler >%s< \n", line);
                } else {
@@ -291,16 +390,16 @@ int main(int argc, char * argv[]) {
          }
 
          // END Marke schreiben
-         if (a < 100 && code != 126) {
+         if (a < max_prg && code != 126) {
                fprintf(file_out, "%c%c", 126, a);
                a++;
          }
 
          if (statflag_verbose_mode)
-                  fprintf (stderr, "\n%d Zeilen\n",a);
+                  fprintf (stderr, "\n%d Zeilen\n",a-startadr);
 
          if (statflag_fill_nop_mode) {
-            while (a < 100) {
+            while (a < max_prg) {
                   fprintf(file_out, "%c%c", 0, 0);
                   a++;
             }
